79292f2e4f7a98e5c9bff1dd5ed01ba63886199d
Savaivanenko <savaivanenko@gmail>
2020-11-01 00:24:16 +0200

    xz

--- src/js/5libraryPage.js
+++ src/js/5libraryPage.js
@@ -1,16 +1,29 @@
 
-import updateDetailsPageMarkUp from './templating';
-import fetchMovieByID from './fetchMovieByID';
+// import fetchMovieTpl from '../templates/fetchMovie.hbs';
+// import fetchMovieByID from './fetchMovieByID';
+// import updateDetailsPageMarkUp from './templating';
 import refs from './refs';
+
+// function openLib(hits) {
+//   const libMarkup =  fetchMovieTpl(hits)
+//   refs.galleryList.innerHTML = libMarkup
+// }
+
+// export default openLib;
+
+
+
+
+
+
 
 const openLib = (event) =>{
   refs.sectionLib.classList.remove('hidden');
   refs.sectionLib.classList.add('visibl');
   refs.searchForm.classList.add('hidden');
   refs.main.classList.add('hidden');
-
   console.log("показываем кнопки");
-
 };
-
 export default openLib;
+
+
