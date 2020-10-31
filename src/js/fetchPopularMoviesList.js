2217fd047ee4249e7bd168171c8e05e7264e30b2
Yevhenii Korkots <atc0884@gmail.com>
2020-10-31 17:52:06 +0200

    before merge

--- src/js/fetchPopularMoviesList.js
+++ src/js/fetchPopularMoviesList.js
@@ -1,17 +1,17 @@
-const apiKey = "2e7ddd707cda482bd62363d7d16dcf77";
+// const apiKey = "2e7ddd707cda482bd62363d7d16dcf77";
 
-function fetchPopularMoviesList(searchQuery) {
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en&query=${searchQuery}`;
-  localStorage.setItem('query', searchQuery)
+// function fetchPopularMoviesList(searchQuery) {
+//   const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en&query=${searchQuery}`;
+//   localStorage.setItem('query', searchQuery)
   
-  return fetch(url)
-  .then(response => response.json())
-  .then(data => data.results)
-  // .then(({ results }) => updateMovieMarkUp(results))
-  .catch(error => console.log(error));
-}
+//   return fetch(url)
+//   .then(response => response.json())
+//   .then(data => data.results)
+//   // .then(({ results }) => updateMovieMarkUp(results))
+//   .catch(error => console.log(error));
+// }
 
-export default fetchPopularMoviesList;
+// export default fetchPopularMoviesList;
 
 // function fetchMovieByID(id) {
 //   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&append_to_response=videos`;
