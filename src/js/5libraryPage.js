286d3d9f778da619058c8752b1738e73635943ba
Savaivanenko <savaivanenko@gmail>
2020-11-01 22:50:37 +0200

    01.11...22:50

--- src/js/5libraryPage.js
+++ src/js/5libraryPage.js
@@ -12,18 +12,24 @@ import refs from './refs';
 // };
 
 function openLib() {
-  refs.homePage.classList.remove('is-hidden');
+  // refs.homePage.classList.remove('is-hidden');
   refs.detailsPage.classList.add('is-hidden');
-  refs.moviesContainer.innerHTML = '';
+  // refs.moviesContainer.innerHTML = '';
   const apiKey = "2e7ddd707cda482bd62363d7d16dcf77";
-  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en&page=1`;
+  // const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en&page=1`;
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&primary_release_year=2010&sort_by=vote_average.desc`;
+  // /discover/movie?primary_release_year=2010&sort_by=vote_average.desc
   fetch(url)
   .then(response => response.json())
     .then(({ results }) => {
-      console.log(results);
+      console.log(results)
 
       const markUp = libPageTempl(results);
       refs.libList.insertAdjacentHTML('beforeend', markUp);
+      refs.sectionLib.classList.add('visible');
+      refs.homePage.classList.add('is-hidden');
+      // refs.searchForm.classList.add('is-hidden');
+      refs.searchForm.classList.add('hidden');
     }
     ) 
     .catch(error => console.log(error));
