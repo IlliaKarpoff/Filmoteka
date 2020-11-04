8f6eb10a5487a1a3cb5df4066444f5153b463485
Savaivanenko <savaivanenko@gmail>
2020-11-04 14:48:04 +0200

    04.11 14:47

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -1,14 +1,14 @@
-import selectMovie from './4filmDetailsPage';
+// import selectMovie from './4filmDetailsPage';
 
-function toStorage (key, value) {
-  window.local.Storage.setItem(key,value)
-}
+// function toStorage (key, value) {
+//   window.local.Storage.setItem(key,value)
+// }
 
-function fromStorage (key) {
-  return window.local.Storage.getItem(key)
-}
+// function fromStorage (key) {
+//   return window.local.Storage.getItem(key)
+// }
 
-console.log(selectMovie);
+// console.log(selectMovie);
 
 
 
