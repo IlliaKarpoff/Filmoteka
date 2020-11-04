99a0186d7bbadb191ba318699e9013c3f517a3f9
Savaivanenko <savaivanenko@gmail>
2020-11-04 16:22:48 +0200

    04.11 16:25

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+'use strict'
 import './sass/main.scss';
 import './js/initialHomePage';
 import './js/searchAndPaginationHomePage';
@@ -5,39 +6,37 @@ import './js/navigation';
 import './js/filmDetailsPage';
 import './js/libraryPage';
 
-
 import refs from './js/refs';
-import handleOpenDetails from './js/filmDetailsPage';
-import openLib from './js/libraryPage';
+// import handleOpenDetails from './js/filmDetailsPage';
+import upButtonHandler from './js/upButton';
+import fetchPopularMoviesList from './js/initialHomePage';
 import searchAndPaginationHomePage from './js/searchAndPaginationHomePage';
 import navigationPages from './js/navigation';
 import activeDetailsPage from './js/filmDetailsPage';
-import fetchPopularMoviesList from './js/initialHomePage';
-import upButtonHandler from './js/upButton';
-
+import openLib from './js/libraryPage';
 
 fetchPopularPage();
 
 function fetchHomePage(event) {
-  // event.preventDefault();
+  event.preventDefault();
   
   const form = event.currentTarget;
   searchAndPaginationHomePage.query = form.elements.query.value;
   
-  form.reset();
+  // form.reset();
   searchAndPaginationHomePage.resetPage();
   navigationPages.activeHomePage();
 };
 
-function fetchPopularPage() {
+function fetchPopularPage(event) {
+  if (event) event.preventDefault();
   fetchPopularMoviesList.resetPage();
   navigationPages.createPopularMovieList();
 }
 
+refs.logoRef.addEventListener('click', fetchPopularPage);
+refs.homeRef.addEventListener('click', navigationPages.activeHomePage);
 refs.lib.addEventListener('click', openLib);
 refs.searchForm.addEventListener('submit', fetchHomePage);
-refs.moviesContainer.addEventListener('click', activeDetailsPage);
-refs.homeRef.addEventListener('click', navigationPages.activeHomePage);
-refs.homePage.addEventListener('click', handleOpenDetails);
+refs.homePage.addEventListener('click', activeDetailsPage);
 refs.upButton.addEventListener('click', upButtonHandler);
-refs.logoRef.addEventListener('click', fetchPopularPage);
