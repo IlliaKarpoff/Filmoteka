0561977fd31820d6e932b2cc8cb1e662db85b31e
Savaivanenko <savaivanenko@gmail>
2020-11-05 03:51:42 +0200

    111

--- src/js/filmDetailsPage.js
+++ src/js/filmDetailsPage.js
@@ -8,18 +8,6 @@ const selectedMovie = {};
 let obj = {};
 
 function selectMovie(event) {
-<<<<<<< HEAD
-    selectFilm.imgPath = event.target.src;
-    selectFilm.title = event.target.alt;
-    selectFilm.id = event.target.dataset.id;
-    selectFilm.year = event.target.dataset.date.split('-')[0];
-    console.log('Selected movie:', selectFilm);
-
-    // if (activeDetailsPage) {
-    //     refs.addToQueueBtn.addEventListener('click', activeDetailsPage);
-    // }
-
-=======
     selectedMovie.id = event.target.dataset.id;
     selectedMovie.imgPath = event.target.src;
     selectedMovie.title = event.target.alt;
@@ -27,7 +15,6 @@ function selectMovie(event) {
     selectedMovie.vote = event.target.dataset.vote;
     obj = {...selectedMovie};
     console.log('Selected movie:', selectedMovie);
->>>>>>> dev
 };
 
 let movieId;
@@ -41,119 +28,30 @@ const activeDetailsPage = (event) => {
 
     selectMovie(event);
 
-<<<<<<< HEAD
-    fetchMovieByID(movieId).
-    then(updateDetailsPageMarkUp);
-    refs.detailsPage.addEventListener('click', onclick);
-    //     updateDetailsPageMarkUp(obj);
-    //     // selectMovie(event)
-    // selectFilm.imgPath = obj.poster_path;
-    // selectFilm.genres = obj.genres;
-    // selectFilm.title = obj.title;
-    // selectFilm.id = obj.id;
-    // selectFilm.year = obj.release_date.split('-')[0];    
-};
-const watchedMovies = [];
-const moviesQueue = [];
-const selectedMovie = {};
-=======
     fetchMovieByID(movieId).then(updateDetailsPageMarkUp);
     refs.detailsPage.addEventListener('click', onclick);
 };
 const watchedMovies = [];
 const moviesQueue = [];
->>>>>>> dev
 
 function onclick(event) {
     if (event.target.classList.contains('addToWatchedBtn')) {
         console.log('Додаємо в переглянуті!');
-<<<<<<< HEAD
-        watchedMovies.push(selectedMovie);
-        console.log(watchedMovies);
-=======
         watchedMovies.push(obj);
         console.log(watchedMovies);
         localStorage.setItem('filmsWatched', JSON.stringify(watchedMovies));
         const parsedWatched = JSON.parse(localStorage.getItem('filmsWatched'));
         console.log('Готовий масив для шаблонізатора:', parsedWatched);
->>>>>>> dev
     }
     if (event.target.classList.contains('addToQueueBtn')) {
         console.log('Цей подивимось на вихідних ;)');
         moviesQueue.push(selectedMovie);
         console.log(moviesQueue);
-<<<<<<< HEAD
-    }
-
-function selectMovie(event) {
-    selectedMovie.id = event.target.dataset.id;
-    selectedMovie.imgPath = event.target.src;
-    selectedMovie.title = event.target.alt;
-    selectedMovie.year = event.target.dataset.date.split('-')[0];
-    selectedMovie.vote = event.target.dataset.vote;
-    console.log('Selected movie:', selectedMovie);
- } };
-
-// function onclick(event) {
-//     if (event.target.classList.contains('addToWatchedBtn')) {
-//         console.log('Додаємо в переглянуті!');
-//     }
-//     if (event.target.classList.contains('addToQueueBtn')) {
-//         console.log('Цей подивимось на вихідних ;)');
-//     }
-// }
-// export 
-// function activeDetailsPage(event, movieId, itsLibraryFilm) {
-//     console.log('selectFilm', selectFilm);
-//     if (event.target.nodeName !== 'IMG') { return }
-//     refs.searchForm.classList.add('is-hidden');
-//     refs.homePage.classList.add('is-hidden');
-//     refs.detailsPage.classList.remove('is-hidden');
-//     refs.libraryPage.innerHTML = '';
-//     refs.detailsPage.innerHTML = '';
-//     refs.moviesContainer.innerHTML = '';
-//     refs.popularPage.innerHTML = '';
-//     refs.btnContainer.classList.add('is-hidden');
-    
-//     // console.log(event.target.src);
-//     // console.log(event.target.alt);
-//     // console.log(event.target.dataset.id);
-//     // console.log(event.target.dataset.date.split('-')[0]);
-    
-//     movieId = event.target.dataset.id;  
-//     console.log('selectFilm.id', selectFilm.id);
-//     // if (itsLibraryFilm) {
-//         if (true) {
-//             selectFilm.id = movieId;
-//             selectFilm.library = true
-//     };
-//         console.log(selectFilm);
-//     fetchMovieByID(movieId).then(showDetails)
-//     .catch(fuckup => console.log(fuckup));
-// };
-// function cb() {
-//     console.log('click!');
-// }
-// function listen() {
-//     refs.addToWatchedBtn.addEventListener('click', cb);
-//     refs.addToQueueBtn.addEventListener('click', cb);
-// }
-// function foo() {
-//     // activeDetailsPage();
-//     // listen();
-// }
-
-
-//    
-
-// export selectMovie;
-=======
         localStorage.setItem('filmsQueue', JSON.stringify(moviesQueue));
         const parsedQueue = JSON.parse(localStorage.getItem('filmsQueue'));
         console.log('Готовий масив для шаблонізатора:', parsedQueue);
     }
 }
->>>>>>> dev
 export default activeDetailsPage;
 
 //- пишем функцию monitorButtonStatusText которая следит за состоянием 
